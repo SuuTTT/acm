a2faf3cb71c3bb79bace26180af4d3f4d99eb320
SuuTTT <1015011749@qq.com>
2020-02-03 23:15:34 +0800

    poj2492

--- templets/dataStructure/disJointSet.cpp
+++ templets/dataStructure/disJointSet.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+//#include<bits/stdc++.h>
+#include<stdio.h>
+#include<vector>
 using namespace std;
 struct DisJointSet{
     vector<int> father,rk;
@@ -26,7 +28,7 @@ struct DisJointSet{
     }
     
 };
-int father[2005],rk[1005];
+int father[4005],rk[4005];
 void init(int n){
     for(int i=0;i<n;i++)father[i]=i;
 }
@@ -35,9 +37,8 @@ void init(int n){
         v:find(father[v]);
     }
 
-    bool merge(int x,int y){
+    void merge(int x,int y){
         int a=find(x),b=find(y);
-        if(a==b)return 0;
         if(rk[a]<rk[b]){
             father[a]=b;
         }else{
@@ -46,7 +47,6 @@ void init(int n){
                 ++rk[a];
             }
         }
-        return 1;
     }
 int main(){
     int n,m;
@@ -55,13 +55,17 @@ int main(){
     scanf("%d",&t);
     while(t--){
         scanf("%d%d",&n,&m);
-        init(n);
+        init(n*2);
         int flag=1;
         for(int i=0;i<m;i++){
             int x,y;
             scanf("%d%d",&x,&y);
-            if(!merge(x,y))flag=0;
+            int a=find(x),b=find(y);
+            if(a==b)flag=0;
+            merge(x,y+n);
+            merge(y,x+n);
         }
+        if(kase>1)printf("\n");
         printf("Scenario #%d:\n",kase++);
         puts(flag?"No suspicious bugs found!"
         :"Suspicious bugs found!");
